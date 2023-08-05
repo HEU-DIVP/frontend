322d2cc302f2b3ccf349170369a1024b2c272e56
YoungFrog <20xzh02@gmail.com>
2023-08-05 09:47:39 +0800

    233

--- globalAPI.js
+++ globalAPI.js
@@ -1,5 +1,5 @@
-// const baseURL = 'http://192.168.158.157:8000'
-const baseURL = 'http://127.0.0.1:8000'
+const baseURL = 'http://192.168.158.157:8000'
+// const baseURL = 'http://127.0.0.1:8000'
 
 export default {
     baseURL
